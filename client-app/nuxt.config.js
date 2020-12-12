08cb76db0e78ea6259c239e3bfe98e036a1e1289
tino.goetz <tino.goetz@mdm.de>
2020-12-13 00:09:56 +0100

    chore: change port for client app to avoid conflict

--- client-app/nuxt.config.js
+++ client-app/nuxt.config.js
@@ -65,5 +65,11 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-  }
+  },
+  /*
+  ** Server configuration
+  */
+  server: {
+    port: 3030,
+  },
 }
